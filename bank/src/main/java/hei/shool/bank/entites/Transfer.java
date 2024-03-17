1db009812297c80349397f4e8167191fd10bd97f
Hevitriniavo <hei.tantely@gmail.com>
2024-03-17 21:05:53 +0100

    feat: created entity Transfer

---
+++ bank/src/main/java/hei/shool/bank/entites/Transfer.java
@@ -0,0 +1,54 @@
+package hei.shool.bank.entites;
+
+import hei.shool.bank.annotations.GeneratedValue;
+import hei.shool.bank.annotations.Id;
+import hei.shool.bank.repositories.Identifiable;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Transfer implements Identifiable<Long> {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long senderAccountId;
+
+    private Long receiverAccountId;
+
+    private Double amount;
+
+    private String reason;
+
+    private LocalDate effectiveDate;
+
+    private LocalDateTime recordDate;
+
+    private String status;
+
+    private String transferType;
+
+    private String referenceNumber;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Override
+    public Long getId() {
+        return id;
+    }
+}
