92639a51da23038083ae57799eba64a2cc0f0efe
Hevitriniavo <hei.tantely@gmail.com>
2024-03-17 21:10:48 +0100

    feat: created entity TransactionHistory

---
+++ bank/src/main/java/hei/shool/bank/entites/TransactionsHistory.java
@@ -0,0 +1,54 @@
+package hei.shool.bank.entites;
+
+import hei.shool.bank.annotations.GeneratedValue;
+import hei.shool.bank.annotations.Id;
+import hei.shool.bank.enums.TransactionType;
+import hei.shool.bank.repositories.Identifiable;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class TransactionsHistory  implements Identifiable<Long> {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long account;
+
+    private Double amount;
+
+    private String reason;
+
+    private LocalDate effectiveDate;
+
+    private LocalDateTime recordDate;
+
+    private TransactionType transactionType;
+
+    private Long categoryId;
+
+    private String operationType;
+
+    private LocalDateTime operationDate;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Override
+    public Long getId() {
+        return id;
+    }
+}
