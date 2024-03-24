015fa3d42a5d4983f49b09548d48e93938b0af4d
Hevitriniavo <hei.tantely@gmail.com>
2024-03-24 08:10:07 +0100

    feat: created table loans

--- bank/src/main/resources/schema.sql
+++ bank/src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 DROP DATABASE IF EXISTS numeric_bank;
+
 CREATE DATABASE numeric_bank;
 
 \c numeric_bank;
 
 CREATE TABLE IF NOT EXISTS banks (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    phone VARCHAR(20),
+    name VARCHAR(255) UNIQUE NOT NULL,
+    code VARCHAR(20),
     email VARCHAR(255) UNIQUE NOT NULL
 );
 
@@ -23,15 +23,14 @@ CREATE TABLE IF NOT EXISTS accounts (
     last_name VARCHAR(255),
     first_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    birthday DATE NOT NULL,
-    balance DECIMAL(10, 2) NOT NULL,
-    net_Monthly_salary REAL DEFAULT NULL,
+    birthday DATE NOT NULL  CHECK (birthday <= current_date - interval '21 years'),
+    balance DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    net_monthly_salary REAL DEFAULT NULL,
     account_number VARCHAR(14) UNIQUE,
-    overdraft_limit DECIMAL(10, 2) ,
     overdraft_enabled BOOLEAN DEFAULT FALSE,
     creation_date DATE DEFAULT CURRENT_DATE,
     last_withdrawal_date DATE,
-    bank_id BIGINT NOT NULL REFERENCES banks(id)  ON DELETE CASCADE ON UPDATE CASCADE
+    bank_solds_id BIGINT NULL REFERENCES bank_solds(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
@@ -39,9 +38,8 @@ CREATE TABLE IF NOT EXISTS transactions (
     account_id BIGINT REFERENCES accounts(id),
     amount DECIMAL(10, 2) NOT NULL,
     reason VARCHAR(255),
-    effective_date DATE NOT NULL,
     transaction_type VARCHAR(50) NOT NULL,
-    category_id BIGINT REFERENCES categories(id)  ON DELETE CASCADE ON UPDATE CASCADE,
+    category_id BIGINT REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE,
     comment VARCHAR(255)
 );
 
@@ -53,16 +51,15 @@ CREATE TABLE IF NOT EXISTS transactions_histories (
 
 CREATE TABLE IF NOT EXISTS transfers (
     id BIGSERIAL PRIMARY KEY,
-    sender_account_id BIGINT REFERENCES accounts(id)  ON DELETE CASCADE ON UPDATE CASCADE,
-    receiver_account_id BIGINT REFERENCES accounts(id)  ON DELETE CASCADE ON UPDATE CASCADE,
+    sender_account_id BIGINT REFERENCES accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    receiver_account_id BIGINT REFERENCES accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    bank_id BIGINT NOT NULL REFERENCES banks(id) ON DELETE CASCADE ON UPDATE CASCADE,
     amount DECIMAL(10, 2) NOT NULL,
     reason VARCHAR(255),
     effective_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(255) DEFAULT 'PENDING',
     reference VARCHAR(255) UNIQUE NOT NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS interests (
     id BIGSERIAL PRIMARY KEY,
@@ -72,13 +69,27 @@ CREATE TABLE IF NOT EXISTS interests (
     interest_date DATE NOT NULL
 );
 
-CREATE SEQUENCE account_id_seq;
-CREATE SEQUENCE transfer_id_seq;
+CREATE TABLE IF NOT EXISTS loans (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    amount DECIMAL(10, 2) NOT NULL,
+    loans_date DATE NOT NULL,
+    status INT NOT NULL CHECK (status IN (0, 100))
+);
 
+
+CREATE TABLE IF NOT EXISTS bank_solds (
+    id BIGSERIAL PRIMARY KEY,
+    value DECIMAL(10, 2) NOT NULL
+);
+
+CREATE SEQUENCE account_id_seq;
+
+CREATE SEQUENCE transfer_id_seq;
 
 CREATE OR REPLACE FUNCTION generate_account_number() RETURNS TRIGGER AS $$
 BEGIN
-    NEW.account_number := 'ABC' || LPAD(CAST(nextval('account_id_seq') AS TEXT), 11, '0');
+    NEW.account_number := 'MOI' || LPAD(CAST(nextval('account_id_seq') AS TEXT), 11, '0');
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -88,10 +99,9 @@ CREATE TRIGGER account_number_trigger
     FOR EACH ROW
     EXECUTE FUNCTION generate_account_number();
 
-
 CREATE OR REPLACE FUNCTION generate_transfer_reference() RETURNS TRIGGER AS $$
 BEGIN
-    NEW.reference := 'TRX' || to_char(current_date, 'YYYYMMDD') || lpad(nextval('transfer_id_seq'), 18, '0');
+    NEW.reference := 'TRX' || to_char(current_date, 'YYYYMMDD') || lpad(nextval('transfer_id_seq')::TEXT, 18, '0');
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -101,17 +111,3 @@ CREATE TRIGGER transfer_reference_trigger
     FOR EACH ROW
     EXECUTE FUNCTION generate_transfer_reference();
 
-
-CREATE OR REPLACE FUNCTION check_scheduled_transfers() RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.effective_date <= CURRENT_TIMESTAMP THEN
-        NEW.status := 'COMPLETED';
-END IF;
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_scheduled_transfers_trigger
-    BEFORE INSERT ON transfers
-    FOR EACH ROW
-    EXECUTE FUNCTION check_scheduled_transfers();
